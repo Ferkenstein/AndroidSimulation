0506e82342eb3f4aedb498a8be7473b6abd9b163
Fernanda Arenas <fernanda.arenas.diaz@gmail.com>
2020-10-24 11:18:49 -0300

    Se agrega el DataBase

---
+++ app/src/main/java/com/example/androidsimulation/model/localRoom/ProductsDataBase.kt
@@ -0,0 +1,34 @@
+package com.example.androidsimulation.model.localRoom
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [ProductsEntity::class, AndroidDetailsEntity::class], version = 1)
+abstract class ProductsDataBase : RoomDatabase()  {
+
+    abstract fun productsDAO(): ProductsDAO
+
+    companion object{
+        @Volatile
+        private var INSTANCE : ProductsDataBase? = null
+
+        fun getDataBase(context: Context): ProductsDataBase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(context,
+                    ProductsDataBase::class.java,
+                    "productsDataBase")
+                    .build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
+
+
